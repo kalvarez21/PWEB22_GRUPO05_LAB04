81495944861ad96fbc023ab8edc469b598dfb02f
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-06-05 00:20:22 -0500

    Resolviendo Ejercicio2f

--- src/Ejercicio2f.py
+++ src/Ejercicio2f.py
@@ -1,2 +1,6 @@
 from interpreter import draw
 from chessPictures import *
+fila = square.join(square.negative()).horizontalRepeat(4)
+fila2 = square.negative().join(square).horizontalRepeat(4)
+unirFilas = fila2.up(fila)
+draw(unirFilas.verticalRepeat(2))
