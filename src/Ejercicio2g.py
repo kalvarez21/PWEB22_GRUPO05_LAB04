2a70bb8c0183a0acd67127253914e3f351e25c75
Kevin Alvarez <kalvarez@unsa.edu.pe>
2022-06-05 00:42:48 -0500

    Resolviendo Ejercicio2g

--- src/Ejercicio2g.py
+++ src/Ejercicio2g.py
@@ -1,2 +1,28 @@
 from interpreter import draw
 from chessPictures import *
+# Fichas blancas
+fichasEspecialesBlancas = rock.join(knight).join(bishop)
+fichasEspecialesBlancas = fichasEspecialesBlancas.join(queen).join(king)
+fichasEspecialesBlancas = fichasEspecialesBlancas.join(bishop).join(knight).join(rock)
+peones = pawn.horizontalRepeat(8)
+fichasBlancas = fichasEspecialesBlancas.up(peones)
+
+
+# Fichas Negras
+fichasEspecialesNegras = fichasEspecialesBlancas.negative()
+peonesNegros = peones.negative()
+fichasNegras = peonesNegros.up(fichasEspecialesNegras)
+
+
+# Par de Filas
+fila = square.join(square.negative()).horizontalRepeat(4)
+fila2 = square.negative().join(square).horizontalRepeat(4)
+unirFilas = fila2.up(fila)
+
+
+# tablero
+zonaFichasNegras = unirFilas.under(fichasNegras)
+zonaFichasBlancas = unirFilas.under(fichasBlancas)
+zonaMedia = unirFilas.verticalRepeat(2)
+tablero = zonaFichasBlancas.up(zonaMedia).up(zonaFichasNegras)
+draw(tablero)
